c8624ed0eb00f6430478a5702b9b5d636d690859
danbigma <danbigma@gmail.com>
2025-06-04 01:42:18 +0200

    Revert "add frontend to app"
    This reverts commit 27b21d88372b70106ed3a3e2b147e896c328d376.

--- dist/app.js
+++ dist/app.js
@@ -1,37 +1,4 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || (function () {
-    var ownKeys = function(o) {
-        ownKeys = Object.getOwnPropertyNames || function (o) {
-            var ar = [];
-            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
-            return ar;
-        };
-        return ownKeys(o);
-    };
-    return function (mod) {
-        if (mod && mod.__esModule) return mod;
-        var result = {};
-        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
-        __setModuleDefault(result, mod);
-        return result;
-    };
-})();
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -46,14 +13,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 var _a, _b;
 Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv = __importStar(require("dotenv"));
-dotenv.config();
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const express_validator_1 = require("express-validator");
 const Registration_1 = __importDefault(require("./models/Registration"));
 const database_1 = __importDefault(require("./db/database"));
-const excelService_1 = require("./services/excelService"); // Импортируем функцию для генерации Excel файла
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cors_1.default)({
@@ -111,18 +75,6 @@ app.post("/registration", [
     catch (error) {
         console.error("Registration failed:", error);
         res.status(500).json({ status: "error", message: "Registration failed" });
-    }
-}));
-app.get('/registrations/excel', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        const buffer = yield (0, excelService_1.generateExcelFile)();
-        res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        res.setHeader('Content-Disposition', 'attachment; filename=registrations.xlsx');
-        res.send(buffer);
-    }
-    catch (error) {
-        console.error("Excel generation failed:", error);
-        res.status(500).json({ status: "error", message: "Excel generation failed" });
     }
 }));
 const PORT = (_b = process.env.PORT) !== null && _b !== void 0 ? _b : 4000;
